0f9d8501fd419cbd45824509e5f7f549a3767386
mange <m.angel-gracia@outlook.com>
2018-09-24 20:11:29 +0200

    Second commit

--- app/src/main/java/com/example/mange/proyectom8/Pantalla1.java
+++ app/src/main/java/com/example/mange/proyectom8/Pantalla1.java
@@ -2,6 +2,12 @@ package com.example.mange.proyectom8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
 
 public class Pantalla1 extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class Pantalla1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla1);
 
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setMessage("Hello world");
+        dialogo1.setCancelable(false);
+        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                finish();
+            }
+        });
+        dialogo1.show();
     }
 }

74c9f3c5cae9eaed89837021697e1bd5c52aa093
mange <m.angel-gracia@outlook.com>
2018-09-24 19:59:11 +0200

    Fisrt commit

---
+++ app/src/main/java/com/example/mange/proyectom8/Pantalla1.java
@@ -0,0 +1,14 @@
+package com.example.mange.proyectom8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla1);
+
+    }
+}
